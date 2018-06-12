37b3f8fb607ddcf0a17d932da134625e190bd6ac
nildiert <39206686+nildiert@users.noreply.github.com>
2018-06-12 16:27:15 -0500

    Agregados Dashboard y login

--- login.php
+++ login.php
@@ -7,60 +7,79 @@ if (isset($_SESSION['mensaje'])) {
 }
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
-
-        <title>
-            KSQ DIGITAL
-        </title>
         <meta charset="utf-8">
-        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-        <link rel="stylesheet" href="plantillas/ejemplo-master/login.css">
-        <script src="plantillas/ejemplo-master/login.js"></script>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title>Su Proyecto</title>
         <script type="text/javascript" src="javascript/funciones.js"></script>
-        <script type="text/javascript" src="javascript/md5.js"></script>     
+        <script type="text/javascript" src="javascript/md5.js"></script>      
+        
+        <link rel="icon" type="image/png" href="plantillas/Login_v17/images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="plantillas/Login_v17/vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="plantillas/Login_v17/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="plantillas/Login_v17/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="plantillas/Login_v17/vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="plantillas/Login_v17/vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="plantillas/Login_v17/vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="plantillas/Login_v17/vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="plantillas/Login_v17/vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="plantillas/Login_v17/css/util.css">
+	<link rel="stylesheet" type="text/css" href="plantillas/Login_v17/css/main.css">
+
     </head>
-
     <body>
+    <div class="container-login100">
+            <div class="wrap-login100">        
+        <form class="login100-form validate-form " role="form" method="GET" action="controladores/ControladorPrincipal.php" name="formLogin">
+            <span class="login100-form-title p-b-34">
+				Ingreso de usuarios
+            </span>
+            <div class="wrap-input100 rs1-wrap-input100 validate-input m-b-20" data-validate="Type user name">            
+                <input id="InputCorreo" class="input100" placeholder="Correo Electrónico" name="email" type="email" autofocus>
+                <span class="focus-input100"></span>
+            </div>
+            <div class="wrap-input100 rs2-wrap-input100 validate-input m-b-20" data-validate="Type password">
+                <input id="InputPassword" class="input100" placeholder="Password" name="password" type="password" value="">
+                <span class="focus-input100"></span>
+                <input type="hidden" name="ruta" value="gestionDeAcceso">
+                <input type="hidden" name="contenido" value="controlarLogin">
+            <!--<p style="color:red;" id="error"></p>-->
+            </div>            
 
+            <div class="container-login100-form-btn">
+                <input type="button" class="login100-form-btn" onclick="validar_logueo()" value="Ingresar">
+            </div>
+            <div class="w-full text-center p-t-27 p-b-239">
+						<span class="txt1">
+					
+						</span>
 
-
-
-
-        <!------ Include the above in your HEAD tag ---------->
-
-        <!--
-            you can substitue the span of reauth email for a input with the email and
-            include the remember me checkbox
-        -->
-        <div class="container">
-            <div class="card card-container">
-                <!-- <img class="profile-img-card" src="//lh3.googleusercontent.com/-6V8xOA6M7BA/AAAAAAAAAAI/AAAAAAAAAAA/rzlHcD0KYwo/photo.jpg?sz=120" alt="" /> -->
-                <img id="profile-img" class="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
-                <p id="profile-name" class="profile-name-card"></p>
-                <form class="form-signin"  method="GET" action="controladores/ControladorPrincipal.php" name="formLogin">
-                    <input type="hidden" name="ruta" value="gestionDeAcceso">
-                    <input type="hidden" name="contenido" value="controlarLogin">                
-                    <span id="reauth-email" class="reauth-email"></span>
-                    <input name="email" type="email" id="inputEmail" class="form-control" placeholder="Usuario" required autofocus>
-                    <input name="password" type="password" id="inputPassword" class="form-control" placeholder="Contraseña" required>
-                    <div id="remember" class="checkbox">
-                        <label>
-                            <input type="checkbox" value="remember-me"> Recordar esta contraseña
-                        </label>
+						<a href="#" class="txt2">
+					
+						</a>
                     </div>
-                    <!--<button class="btn btn-lg btn-primary btn-block btn-signin" type="submit">Ingresar</button>--> 
-                    <input type="button" class="btn btn-lg btn-success btn-block" onclick="validar_logueo()" value="Ingresar">
-                    <!--<input type="submit" class="btn btn-lg btn-success btn-block"  value="Ingresar">-->
-                
-                
-                </form><!-- /form -->
-                <a href="registro.php" class="forgot-password">
-                    Registrese Aquí
-                </a>
-            </div><!-- /card-container -->
-        </div><!-- /container -->
-    </body>
+                    <div class="w-full text-center">
+						<a href="#" class="txt3">
+					
+						</a>
+					</div>                    
+    </form>
+    <div class="login100-more" style="background-image: url('plantillas/Login_v17/images/bg-01.jpg');"></div>
+			</div>
+    </div>
+</div>
+</body>
 </html>
