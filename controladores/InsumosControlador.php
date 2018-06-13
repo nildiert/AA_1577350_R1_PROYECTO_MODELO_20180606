bdde650a7cf8861324a0147dbd018bf51373a624
nildiert <niljordan23@gmail.com>
2018-06-13 12:40:12 -0500

    Agregado el controlador de insumos sin que afecte las otras tablas(Las de libros)

--- controladores/InsumosControlador.php
+++ controladores/InsumosControlador.php
@@ -14,10 +14,10 @@ class InsumosControlador {
         $this->datos = $datos;
     }
 
-    public function librosControlador() {
+    public function InsumosControlador() {
 
         switch ($this->datos["ruta"]) {
-            case "listarLibros":
+            case "listarInsumos":
 
                 if (isset($this->datos['pag']) && (int) $this->datos['pag'] > 0) {
                     $pagInicio = $this->datos['pag'];
@@ -27,65 +27,62 @@ class InsumosControlador {
                 $limit = 5;
 
                 $usuarioBd = new UsuarioBd(USUARIO_BD, CONTRASENIA_BD);
-                $consultarLibro = new LibroVO();
+                $consultarInsumos = new InsumosVO();
 
-                $gestarLibros = new LibroDAO($usuarioBd, BASE, SERVIDOR);
-                $resultadoConsultaPaginada = $gestarLibros->consultaPaginada($consultarLibro, $limit, $pagInicio);
+                $gestarInsumos = new InsumosDAO($usuarioBd, BASE, SERVIDOR);
+                $resultadoConsultaPaginada = $gestarInsumos->consultaPaginada($consultarInsumos, $limit, $pagInicio);
                 $totalRegistros = $resultadoConsultaPaginada[0];
-                $listaDeLibros = $resultadoConsultaPaginada[1];
-                $paginacionVinculos = $gestarLibros->enlacesPaginacion($totalRegistros, $limit, $pagInicio);
+                $listaDeInsumos = $resultadoConsultaPaginada[1];
+                $paginacionVinculos = $gestarInsumos->enlacesPaginacion($totalRegistros, $limit, $pagInicio);
 
-                $gestarCategoriasLibros = new CategoriaLibroDAO($usuarioBd, BASE, SERVIDOR);
-                $registroCategoriasLibros = $gestarCategoriasLibros->seleccionarTodos(); /*                 * *********** */
+                $gestarCategoriasInsumos = new CategoriaInsumosDAO($usuarioBd, BASE, SERVIDOR);
+                $registroCategoriasInsumos = $gestarCategoriasInsumos->seleccionarTodos(); /*                 * *********** */
 
                 session_start();
-                $_SESSION['listaDeLibros'] = $listaDeLibros;
+                $_SESSION['listaDeInsumos'] = $listaDeInsumos;
                 $_SESSION['paginacionVinculos'] = $paginacionVinculos;
                 $_SESSION['totalRegistros'] = $totalRegistros;
-                $_SESSION['registroCategoriasLibros'] = $registroCategoriasLibros; /*                 * *********** */
+                $_SESSION['registroCategoriasInsumos'] = $registroCategoriasInsumos; /*                 * *********** */
                 //se almacenan en sesion las variables del filtro
-                $_SESSION['isbnF'] = (isset($this->datos['isbn'])) ? $this->datos['isbn'] : NULL;
-                $_SESSION['tituloF'] = (isset($this->datos['titulo'])) ? $this->datos['titulo'] : NULL;
-                $_SESSION['autorF'] = (isset($this->datos['autor'])) ? $this->datos['autor'] : NULL;
-                $_SESSION['precioF'] = (isset($this->datos['precio'])) ? $this->datos['precio'] : NULL;
-                $_SESSION['catLibIdF'] = (isset($this->datos['catLibId'])) ? $this->datos['catLibId'] : NULL;
-                $_SESSION['catLibNombreF'] = (isset($this->datos['catLibNombre'])) ? $this->datos['catLibNombre'] : NULL;
+                $_SESSION['InsCodigoF'] = (isset($this->datos['InsCodigo'])) ? $this->datos['InsCodigo'] : NULL;
+                $_SESSION['InsNombreF'] = (isset($this->datos['InsNombre'])) ? $this->datos['InsNombre'] : NULL;
+                $_SESSION['InsUnidadMedidaF'] = (isset($this->datos['InsUnidadMedida'])) ? $this->datos['InsUnidadMedida'] : NULL;
+                $_SESSION['InsPrecioF'] = (isset($this->datos['InsPrecio'])) ? $this->datos['InsPrecio'] : NULL;
                 $_SESSION['buscarF'] = (isset($this->datos['buscar'])) ? $this->datos['buscar'] : NULL;
 
 
 
                 $usuarioBd = null;
-                $gestarLibros = null;
-                header("location: ../principal.php?contenido=vistas/vistasLibros/listarRegistrosLibros.php");
+                $gestarInsumos = null;
+                header("location: ../principal.php?contenido=vistas/vistasInsumos/listarRegistrosInsumos.php");
                 break;
 
-            case "insertarLibro":
+            case "insertarInsumos":
                 $usuarioBd = new UsuarioBd(USUARIO_BD, CONTRASENIA_BD);
-                $gestarLibros = new LibroDAO($usuarioBd, BASE, SERVIDOR);
-//                $insertarLibro = new LibroVO();
-                $existeLibro = $gestarLibros->seleccionarId(array($this->datos["isbn"])); //Se revisa si existe el libro en la base
-                if (empty($existeLibro['registroEncontrado'])) {//Si no existe el libro en la base se procede a insertar
-                    $insertoLibro = $gestarLibros->insertar($this->datos); //inserción de los campos en la tabla libros
-                    $exitoInsercionLibro = $insertoLibro['inserto']; //indica si se logró inserción de los campos en la tabla libros
-                    $resultadoInsercionLibro = $insertoLibro['resultado']; //Traer el id con que quedó el usuario de lo contrario la excepción o fallo
+                $gestarInsumos = new InsumosDAO($usuarioBd, BASE, SERVIDOR);
+//                $insertarInsumos = new InsumosVO();
+                $existeInsumos = $gestarInsumos->seleccionarId(array($this->datos["InsCodigo"])); //Se revisa si existe el Insumos en la base
+                if (empty($existeInsumos['registroEncontrado'])) {//Si no existe el Insumos en la base se procede a insertar
+                    $insertoInsumos = $gestarInsumos->insertar($this->datos); //inserción de los campos en la tabla Insumos
+                    $exitoInsercionInsumos = $insertoInsumos['inserto']; //indica si se logró inserción de los campos en la tabla Insumos
+                    $resultadoInsercionInsumos = $insertoInsumos['resultado']; //Traer el id con que quedó el usuario de lo contrario la excepción o fallo
 //                    if (1 == $exitoInsercionUsuario_s) {//si se logró la inserción de los campos en la tabla usuario_s insertar datos en tabla persona
                     session_start(); //se abre sesión para almacenar en ella el mensaje de inserción
-                    $_SESSION['mensaje'] = "Registrado con èxito. Agregado Nuevo Libro"; //mensaje de inserción
-                    if ($this->datos['ruta'] == 'insertarLibro') {//si el formulario de la inserción es el de Agregar Libros y fue exitoso se devuelve a listarRegistrosLibros.php
-//                        header("location:../principal.php?contenido=vistas/vistasLibros/listarRegistrosLibros.php");
-//                        header("refresh:2;url=../principal.php?contenido=vistas/vistasLibros/listarRegistrosLibros.php");
-                        header("location:../principal.php?contenido=vistas/vistasLibros/listarRegistrosLibros.php");
+                    $_SESSION['mensaje'] = "Registrado con èxito. Agregado Nuevo Insumos"; //mensaje de inserción
+                    if ($this->datos['ruta'] == 'insertarInsumos') {//si el formulario de la inserción es el de Agregar Insumos y fue exitoso se devuelve a listarRegistrosInsumos.php
+//                        header("location:../principal.php?contenido=vistas/vistasInsumos/listarRegistrosInsumos.php");
+//                        header("refresh:2;url=../principal.php?contenido=vistas/vistasInsumos/listarRegistrosInsumos.php");
+                        header("location:../principal.php?contenido=vistas/vistasInsumos/listarRegistrosInsumos.php");
                     }
-                } else {//Si el libro ya existe devolver datos al formulario por medio de la sesión
+                } else {//Si el Insumos ya existe devolver datos al formulario por medio de la sesión
                     session_start();
-                    $_SESSION['isbn'] = $this->datos['isbn'];
-                    $_SESSION['titulo'] = $this->datos['titulo'];
-                    $_SESSION['autor'] = $this->datos['autor'];
-                    $_SESSION['precio'] = $this->datos['precio'];
-                    $_SESSION['categoriaLibro_catLibId'] = $this->datos['categoriaLibro_catLibId'];
-                    $_SESSION['mensaje'] = "El Libro ya existe en el sistema.";
-                    if ($this->datos['ruta'] == 'insertarLibro') {//si al insertar un usuario en el formulario de Agregar nuevo usuario y éste ya existe a listarRegistrosUsuario_s.php
-                        header("location:../principal.php?contenido=vistas/vistasLibros/vistaInsertarLibro.php");
+                    $_SESSION['InsCodigo'] = $this->datos['InsCodigo'];
+                    $_SESSION['InsNombre'] = $this->datos['InsNombre'];
+                    $_SESSION['InsUnidadMedida'] = $this->datos['InsUnidadMedida'];
+                    $_SESSION['InsPrecio'] = $this->datos['InsPrecio'];
+                    $_SESSION['mensaje'] = "El Insumos ya existe en el sistema.";
+                    if ($this->datos['ruta'] == 'insertarInsumos') {//si al insertar un usuario en el formulario de Agregar nuevo usuario y éste ya existe a listarRegistrosUsuario_s.php
+                        header("location:../principal.php?contenido=vistas/vistasInsumos/vistaInsertarInsumos.php");
                     }
                 }
                 break;
